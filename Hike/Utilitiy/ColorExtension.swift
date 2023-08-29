a55c1798f407f60e12d44599c68f757cc0ea8e21
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-08-29 00:14:26 -0400

    created a lot of components

--- Hike/Utilitiy/ColorExtension.swift
+++ Hike/Utilitiy/ColorExtension.swift
@@ -13,6 +13,7 @@ extension Color {
     static let customGreenMedium = Color("ColorGreenMedium")
     static let customGreenDark = Color("ColorGreenDark")
     static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
     static let customIndigoMedium = Color("ColorIndigoMedium")
     static let customSalmonRight = Color("ColorSalmonRight")
     
